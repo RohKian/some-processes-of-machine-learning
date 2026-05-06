04e5be14079c029234ab4a692f73a6ca1715dc0d
RohKian <2071251418@qq.com>
2026-05-06 14:38:48 +0800

    add a file

---
+++ day08线性回归模型在wave数据集上的运用.py
@@ -0,0 +1,13 @@
+import mglearn
+import matplotlib.pyplot as plt
+mglearn.plots.plot_linear_regression_wave()
+plt.show()
+
+'''
+    从wave[0]可以看出，斜率为0.393906，大概在0.4左右，截距为-0.031804，
+    
+    用于回归的线性模型可以表示为这样的回归模型：对单一特征的预测结果是一条直线，
+    两个特征是一个平面，或在更高维度（即更多特征）时是一个超平面。
+    
+    线性回归，即使用最小二乘法寻找参数 W 和 
+'''
