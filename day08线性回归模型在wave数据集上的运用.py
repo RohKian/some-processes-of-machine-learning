a09a2136779dde5909a9f012bc47a247657613e8
RohKian <2071251418@qq.com>
2026-05-06 20:33:49 +0800

    changed a file

--- day08线性回归模型在wave数据集上的运用.py
+++ day08线性回归模型在wave数据集上的运用.py
@@ -1,5 +1,8 @@
 import mglearn
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
 mglearn.plots.plot_linear_regression_wave()
 plt.show()
 
@@ -9,5 +12,17 @@ plt.show()
     用于回归的线性模型可以表示为这样的回归模型：对单一特征的预测结果是一条直线，
     两个特征是一个平面，或在更高维度（即更多特征）时是一个超平面。
     
-    线性回归，即使用最小二乘法寻找参数 W 和 
+    线性回归，即使用最小二乘法寻找参数 W 和 b，使得对训练集的预测值与真实的回归目标值 y 之间的均方误差
+    是预测值与真实值之差的平方和除以样本数。
 '''
+x, y = mglearn.datasets.make_wave(n_samples = 60)
+X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=42)
+lr = LinearRegression().fit(X_train, y_train)
+
+'''
+    斜率参数（w，也叫作权重或系数）被保存在coef_属性中，
+    而偏移或截距(b)被保存在intercept_属性中。
+'''
+
+print('lr.coef_:{}'.format(lr.coef_))
+print('lr.intercept_:{}'.format(lr.intercept_))
