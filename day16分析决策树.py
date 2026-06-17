284f54ed3516d5b4e646a98c3709022687496283
RohKian <2071251418@qq.com>
2026-06-17 13:49:38 +0800

    changed a file

--- day16分析决策树.py
+++ day16分析决策树.py
@@ -3,7 +3,10 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.tree import export_graphviz
+import matplotlib.pyplot as plt
+import numpy as np
 import graphviz
+import mglearn
 
 cancer = load_breast_cancer()
 X_train, X_test, y_train, y_test = train_test_split(
@@ -45,4 +48,32 @@ graphviz.Source(dot_graph)
     其中最常用的是特征重要性。决策树的特征重要性为每个特征对树的决策的重要性进行排序。对
     于每个特征来说，它都是一个介于 0 和 1 之间的数字，其中 0 表示 “根本没用到”， 1 表示
     “完美预测目标值”。特征重要性的求和始终为 1。
+'''
+print('Feature importance：\n{}'.format(tree.feature_importances_))
+
+# 可视化特征重要性
+def plot_feature_importance_cancer(model):
+    n_features = cancer['data'].shape[1]
+    plt.barh(range(n_features),
+             model.feature_importances_,
+             align= 'center')
+    plt.yticks(np.arange(n_features),
+               cancer.feature_names)
+    plt.xlabel('Feature importance')
+    plt.ylabel('Feature')
+    plt.show()
+
+plot_feature_importance_cancer(tree)
+tree = mglearn.plots.plot_tree_not_monotone()
+plt.show()
+
+'''
+    决策树有两个优点：
+    1：得到的模型很容易可视化
+    2：算法完全不受数据缩放的影响。由于每个特征被单独处理，而且数据的划分也不依赖于缩放，
+       因此决策树算法不需要特征预处理，比如归一化或标准化。
+       特征的尺度完全不一样时或者二元特征和连续特征同时存在时，决策树的效果很好。
+    
+    决策树的主要缺点在于：即使做了预剪枝，也经常会出现过拟合，泛化性能很差。
+    通常用 随机森林 和 梯度回归树（梯度提升机）集成的方法来替代单棵决策树。
 '''
